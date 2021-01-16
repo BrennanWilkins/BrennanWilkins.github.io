398c3822d8fbc5fd0f3c9643c7301a9c69b25988
Brennan Wilkins <bwilkins767@gmail.com>
2021-01-16 10:19:26 -0600

    Added skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Skills.module.css';
+import { htmlIcon, cssIcon, jsIcon, reactIcon, reduxIcon, nodeJSIcon, mongoDBIcon, gitIcon } from '../icons';
+import PropTypes from 'prop-types';
+
+const icons = [[htmlIcon, 'HTML'], [cssIcon, 'CSS3'], [jsIcon, 'JavaScript'], [reactIcon, 'React'],
+[reduxIcon, 'Redux'], [gitIcon, 'git'], [nodeJSIcon, 'Node.js'], [mongoDBIcon, 'MongoDB']];
+
+const Skills = React.forwardRef((props, ref) => (
+  <div ref={ref}>
+    <h1>Technologies I use</h1>
+    <div className={`${classes.Techs} ${props.startAnim ? classes.TechsAnim : ''}`}>
+      {icons.map((icon, i) => (
+        <div className={classes.Tech} key={i}>
+          <div className={`${classes.TechIcon} ${(i === 6 || i === 7) ? classes.TechIconStretch : ''}`} style={{ transitionDelay: String(500 + (i * 50)) + 'ms' }}>{icon[0]}</div>
+          <span className={classes.TechName}>{icon[1]}<span className={classes.FocusBorder}></span></span>
+        </div>
+      ))}
+    </div>
+  </div>
+));
+
+Skills.propTypes = {
+  startAnim: PropTypes.bool.isRequired
+};
+
+export default Skills;
